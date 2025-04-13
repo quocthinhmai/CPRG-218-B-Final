db2d6fc6fa77ea2c7d46176d298a171bfcfd3039
quocthinhmai <quocthinh.mai@edu.sait.ca>
2025-04-13 15:36:51 -0600

    add homepage for with responsive website
    - Add Homepage with full HTML, CSS and javascript
    
    - Make the homepage responsive for multiple devices

---
+++ weather.js
@@ -0,0 +1,8 @@
+fetch("https://api.openweathermap.org/data/2.5/weather?q=cancun&appid=02eb6600162720bd99d4e318bd1f7bfb&units=metric")
+.then(Response => Response.json())
+.then(data =>{
+    console.log(data);
+    console.log(data.main.temp);
+    temperature.textContent = "Temperature " + data.main.temp +"\u00b0C";
+    feelslike.textContent = "Feels Like: " + data.main.feels_like +"\u00b0C";
+}); 
